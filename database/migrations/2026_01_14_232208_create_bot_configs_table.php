df11ac09bb5976e523ed5e5281aba578c9557d10
Jonagon16 <jonadanielgonzalez@gmail.com>
2026-01-15 21:13:53 -0300

    Fix bot_config foreign key type

--- database/migrations/2026_01_14_232208_create_bot_configs_table.php
+++ database/migrations/2026_01_14_232208_create_bot_configs_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('bot_config', function (Blueprint $table) {
             $table->id();
-            $table->foreignUuid('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('user_id')
+          ->constrained('users')
+          ->onDelete('cascade');
             
             $table->text('whatsapp_verify_token');
             $table->text('whatsapp_access_token');
